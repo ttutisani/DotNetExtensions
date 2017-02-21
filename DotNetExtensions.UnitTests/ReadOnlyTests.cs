c0361c7cf627095de5ff4a696b68bbd0a7ce53cf
Tengiz <tengo_tutisani@hotmail.com>
2017-02-20 21:45:49 -0600

    readonly vars.

---
+++ DotNetExtensions.UnitTests/ReadOnlyTests.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace DotNetExtensions.UnitTests
+{
+    public sealed class ReadOnlyTests
+    {
+        [Fact]
+        public void Ctor_Without_Args_Sets_Value_To_Default()
+        {
+            //act.
+            var sut = new ReadOnly<int>();
+
+            //assert.
+            sut.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void Ctor_With_Arg_Sets_Value_To_Arg()
+        {
+            //arrange.
+            var expectedValue = 123;
+
+            //act.
+            var sut = new ReadOnly<int>(expectedValue);
+
+            //assert.
+            sut.Value.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void Conversion_Gets_Original_Value()
+        {
+            //arrange.
+            var originalValue = 321;
+            var sut = new ReadOnly<int>(originalValue);
+
+            //act.
+            int convertedValue = sut;
+
+            //assert.
+            convertedValue.Should().Be(originalValue);
+        }
+
+        [Fact]
+        public void Static_Value_Creates_Readonly_By_Value()
+        {
+            //arrange.
+            var expectedValue = 1616;
+
+            //act.
+            var sut = ReadOnly.Value(expectedValue);
+
+            //assert.
+            sut.Value.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void AsReadOnly_Creates_ReadOnly_By_Value()
+        {
+            //arrange.
+            var expectedValue = 5541;
+
+            //act.
+            var sut = expectedValue.AsReadOnly();
+
+            //assert.
+            sut.Value.Should().Be(expectedValue);
+        }
+    }
+}
---
+++ DotNetExtensions/ReadOnly.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNetExtensions
+{
+    public struct ReadOnly<T>
+    {
+        public ReadOnly(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+
+        public static implicit operator T(ReadOnly<T> readOnly)
+        {
+            return readOnly.Value;
+        }
+    }
+
+    public static class ReadOnly
+    {
+        public static ReadOnly<T> Value<T>(T value)
+        {
+            return new ReadOnly<T>(value);
+        }
+
+        public static ReadOnly<T> AsReadOnly<T>(this T value)
+        {
+            return new ReadOnly<T>(value);
+        }
+    }
+}
